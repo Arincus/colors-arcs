91339cc86bf3d0877b52dbef86536c63aeee7a0a
Arina <arina.o.petrova@gmail.com>
2023-11-29 22:10:40 +1000

    colors arcs

---
+++ color.py
@@ -0,0 +1,107 @@
+import sys
+import random
+
+from PyQt5.QtGui import QPainter, QColor
+from PyQt5.QtWidgets import QWidget, QApplication, QPushButton, QMainWindow, QListWidget
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(873, 600)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(370, 510, 131, 41))
+        font = QtGui.QFont()
+        font.setPointSize(20)
+        font.setItalic(False)
+        font.setStrikeOut(False)
+        self.pushButton.setFont(font)
+        self.pushButton.setObjectName("pushButton")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 873, 21))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.pushButton.setText(_translate("MainWindow", "рисовать"))
+
+
+class MyWidget(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setupUi(self)
+        self.do_paint = False
+        self.pushButton.clicked.connect(self.paint)
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw_flag(qp)
+            qp.end()
+        self.do_paint = False
+
+    def paint(self):
+        self.do_paint = True
+        self.update()
+
+    def draw_flag(self, qp):
+        a = random.randint(0, 600)
+        s = random.randint(0, 500)
+        r = random.randint(0, 255)
+        g = random.randint(0, 255)
+        b = random.randint(0, 255)
+        qp.setPen(QColor(r, g, b))
+        qp.drawArc(a, a, s, s, 0, 90 * 90)
+
+        a = random.randint(0, 600)
+        s = random.randint(0, 500)
+        r = random.randint(0, 255)
+        g = random.randint(0, 255)
+        b = random.randint(0, 255)
+        qp.setPen(QColor(r, g, b))
+        qp.drawArc(a, a, s, s, 0, 90 * 90)
+
+        a = random.randint(0, 600)
+        s = random.randint(0, 500)
+        r = random.randint(0, 255)
+        g = random.randint(0, 255)
+        b = random.randint(0, 255)
+        qp.setPen(QColor(r, g, b))
+        qp.drawArc(a, a, s, s, 0, 90 * 90)
+
+        a = random.randint(0, 600)
+        s = random.randint(0, 500)
+        r = random.randint(0, 255)
+        g = random.randint(0, 255)
+        b = random.randint(0, 255)
+        qp.setPen(QColor(r, g, b))
+        qp.drawArc(a, a, s, s, 0, 90 * 90)
+
+        a = random.randint(0, 600)
+        s = random.randint(0, 500)
+        r = random.randint(0, 255)
+        g = random.randint(0, 255)
+        b = random.randint(0, 255)
+        qp.setPen(QColor(r, g, b))
+        qp.drawArc(a, a, s, s, 0, 90 * 90)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec_())
